18711746210433e758e087d05210200b213ff550
boscha1 <bosch.anthony7@gmail.com>
2023-07-15 12:50:01 -0400

    populating the breaking news fragment

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragment/BreakingNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragment/BreakingNewsFragment.kt
@@ -1,7 +1,61 @@
 package com.androiddevs.mvvmnewsapp.ui.fragment
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.androiddevs.mvvmnewsapp.R
+import com.androiddevs.mvvmnewsapp.adapters.NewsAdapter
+import com.androiddevs.mvvmnewsapp.ui.NewsActivity
+import com.androiddevs.mvvmnewsapp.util.Resource
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
 class BreakingNewsFragment: BaseNewsFragment(R.layout.fragment_breaking_news) {
+    // responsible for populating the RecyclerView with news articles
+    lateinit var newsAdapter: NewsAdapter
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = (activity as NewsActivity).viewModel
+        setUpRecyclerView()
+
+        viewModel.breakingNewsLiveData.observe(viewLifecycleOwner, Observer {response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let {newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error occurred: $message")
+                    }
+                }
+                is Resource.Loading -> showProgressBar()
+            }
+        })
+    }
+
+    private fun setUpRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvBreakingNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+    }
+
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    companion object {
+        const val TAG = "BreakingNewsFragment"
+    }
 }
