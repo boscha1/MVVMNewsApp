cb29b21181723a121465a80e0ab5c8e9e8fe834e
boscha1 <bosch.anthony7@gmail.com>
2023-07-14 22:52:56 -0400

    init

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "1fdebf2aad4d4c5485187e9259abda25"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
